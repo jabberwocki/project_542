3e77d4c012a807b9bf9c4f1aaad343161e39a0d9
jabberwocki <jaaron.britton@gmail.com>
2015-07-16 13:43:08 -0400

    getting "KeyError: 'data'"

--- search.py
+++ search.py
@@ -1,18 +1,23 @@
 from urllib2 import urlopen
 from json import load
 
-apiUrl = "http://dailymed.nlm.nih.gov/dailymed/services/v2/spls"
-apiParam = "dea_schedule_code/C48675"
-outputFormat = "?format=json"
+apiUrl = "http://dailymed.nlm.nih.gov/dailymed/services/v2/spls.json"
 
-response = urlopen(apiUrl + apiParam + outputFormat)
+print apiUrl
 
+response = urlopen(apiUrl)
 json_obj = load(response)
 
+for drug in json_obj["data"]:
+	print drug["data"]
+
+
+
+
 #Load the Result from the JSON response
-print "---------"
-print    Result
-print "---------"
+#print "---------"
+#print    Results
+#print "---------"
 #for objectCollection in json_obj['Results']:
 #	for drug_nameAttribute, drug_nameValue in objectCollection.iteritems():
 #		print drug_nameAttribute, ": ", drug_nameValue
